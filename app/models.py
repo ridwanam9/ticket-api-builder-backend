b3b5065daab2aa549c1b0d2680c5fbde0ba22266
ridwanam9 <ridwanamirulmaulana8@gmail.com>
2025-08-01 07:49:20 +0700

    add ticket model

--- app/models.py
+++ app/models.py
@@ -24,44 +24,20 @@ class User(db.Model):
         } 
 
 
-class Product(db.Model):
-    __tablename__ = 'products'
+class Ticket(db.Model):
+    __tablename__ = 'tickets'
+
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120))
-    description = db.Column(db.Text)
-    price = db.Column(db.Float)
-    stock = db.Column(db.Integer)
+    event_name = db.Column(db.String(100), nullable=False)
+    location = db.Column(db.String(100), nullable=False)
+    time = db.Column(db.DateTime, nullable=False)
+    is_used = db.Column(db.Boolean, default=False)
 
     def to_dict(self):
         return {
             "id": self.id,
-            "name": self.name,
-            "description": self.description,
-            "price": self.price,
-            "stock": self.stock
+            "eventName": self.event_name,
+            "location": self.location,
+            "time": self.time.isoformat(),
+            "isUsed": self.is_used
         }
-
-class Transaction(db.Model):
-    __tablename__ = 'transactions'
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    product_id = db.Column(db.Integer, db.ForeignKey('products.id'))
-    quantity = db.Column(db.Integer)
-    total_price = db.Column(db.Float)
-    timestamp = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
-
-    user = db.relationship('User', backref=db.backref('transactions', lazy=True))
-    product = db.relationship('Product', backref=db.backref('transactions', lazy=True))
-
-    def to_dict(self):
-        return {
-            "id": self.id,
-            "user_id": self.user_id,
-            "product_id": self.product_id,
-            "quantity": self.quantity,
-            "total_price": self.total_price,
-            "timestamp": self.timestamp.isoformat() if self.timestamp else None,
-            "user": self.user.to_dict() if self.user else None,
-            "product": self.product.to_dict() if self.product else None
-        }
-
